f7e88695085a3cd6e24785acfa89a4e765bef9d8
Srikanth63 <95394693+Srikanth63@users.noreply.github.com>
2021-12-20 14:54:37 +0800

    Smallest

---
+++ pythonProject1/Assign.py
@@ -0,0 +1,9 @@
+#finding the smallest number from given two.
+#using input function.
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+#using If condition here.
+if n1<n2:
+    print("{} is smallest".format(n1))
+else:
+    print("{} is smallest".format(n2))
--- pythonProject1/List.py
+++ pythonProject1/List.py
@@ -1,3 +1,1 @@
-List=[10,20,30]
-List[0]
-print(List[0])
+
