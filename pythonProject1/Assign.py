25ff097b84b56f311c38f00b0ea0fb0c6d4f6394
Srikanth63 <95394693+Srikanth63@users.noreply.github.com>
2021-12-20 15:13:24 +0800

    Smallest

--- pythonProject1/Assign.py
+++ pythonProject1/Assign.py
@@ -1,9 +1,12 @@
-#finding the smallest number from given two.
+#finding the smallest number from given three.
 #using input function.
 n1=int(input("enter the first number:"))
 n2=int(input("enter the second number:"))
-#using If condition here.
-if n1<n2:
-    print("{} is smallest".format(n1))
+n3=int(input("enter the second number:"))
+#using If condition here.,
+if (n1<=n2 and n1<=n3):
+    print(n1,"is the smallest num")
+elif(n2<=n1 and n2<=n3):
+    print(n2, "is the smallest num")
 else:
-    print("{} is smallest".format(n2))
+    print(n3,"is the smallest num")
